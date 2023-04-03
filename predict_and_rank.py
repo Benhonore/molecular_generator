643c4830206505934316dbfdf7cbfc377f48a62c
ben <benhonore@LAPTOP-NGR6GB2F.localdomain>
2023-04-03 15:46:18 +0100

    fixing issue with duplicate errors - use dataframe instead

--- predict_and_rank.py
+++ predict_and_rank.py
@@ -41,37 +41,27 @@ def rank_structures(mols): # Current botch: zero all of the 3D relevant edge fea
 
     h_error = []
     c_error = []
-    indices = []
+    h_d = {}
+    c_d = {}
 
-    for pred in range(len(preds)): # Loop through output graphs, pulling out the predictions for H & C
+    for pred in preds: # Loop through output graphs, pulling out the predictions for H & C
 
-        prediction, truth = lossfn.get_outputs(preds[pred], 'HCS')
+        prediction, truth = lossfn.get_outputs(pred, 'HCS')
         pred_y = scl.descale_and_denorm(prediction.numpy().squeeze(), train_model.args['scaling']['HCS'])
-        indices.append(pred)
         h_error.append(mae(pred_y, truth.numpy()))
-        prediction, truth = lossfn.get_outputs(preds[pred], 'CCS')
+        h_d[mae(pred_y, truth.numpy())] = pred
+        prediction, truth = lossfn.get_outputs(pred, 'CCS')
         pred_y = scl.descale_and_denorm(prediction.numpy().squeeze(), train_model.args['scaling']['CCS'])
         c_error.append(mae(pred_y, truth.numpy()))
+        c_d[mae(pred_y, truth.numpy())] = pred
 
     if c_error.index(np.min(c_error)) == h_error.index(np.min(h_error)):
          print('Final Structure..')
          mm.draw_graph_connectivity(preds[c_error.index(np.min(c_error))]) # IF the lowest error agrees for H & C
     
-    h_d = {}
-    hcopy = h_error.copy()
-    h_error.sort()
+    df =pd.DataFrame()
+    df['graphs'] = preds
+    df['h_error'] = h_error
+    df['c_error'] = c_error
 
-    for i, error in enumerate(h_error):
-         index = hcopy.index(error) # This just finds the index in the of this error in the original list of errors
-         h_d[i] = [error, preds[index]] # The rank (i) is the dictionary key - the values are error and graph
-
-    c_d = {}
-    ccopy = c_error.copy()
-    c_error.sort()
-
-    for i, error in enumerate(c_error):
-         index = ccopy.index(error)
-         c_d[i] = [error, preds[index]]
- 
-
-    return h_d, c_d # Ranked dictionaries for proton and carbon are returned
+    return df   # A dataframe of graph | proton_error | carbon_error is returned
